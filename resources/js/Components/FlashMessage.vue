02e36c075d955e342db99d49d2794a294a9cd3c7
tack-k <tyool9c@gmail.com>
2021-12-26 18:17:42 +0900

    feat:フラッシュメッセージを受け取って表示する

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="message">
+        <ul class="flex items-center justify-center bg-gray-600 text-white">
+            <li>{{ message }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {usePage} from "@inertiajs/inertia-vue3";
+import {computed} from "vue";
+
+export default {
+    name: "FlashMessage",
+
+    setup() {
+         const message = computed(() => usePage().props.value.flash.message)
+
+        return {
+            message,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,6 +8,8 @@
             </h2>
         </template>
 
+        <FlashMessage/>
+
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -23,9 +25,11 @@
 <script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head } from '@inertiajs/inertia-vue3';
+import FlashMessage from "@/Components/FlashMessage";
 
 export default {
     components: {
+        FlashMessage,
         BreezeAuthenticatedLayout,
         Head,
     },
